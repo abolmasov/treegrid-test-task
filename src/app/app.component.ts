08799e5b7f73ea56f4eba84b6bf3089df5b90127
Roman <sillencium@gmail.com>
2021-10-15 16:56:12 +0300

    Add custom menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { sampleData } from '../assets/mock/data';
 import {
   SortService,
@@ -9,8 +9,12 @@ import {
   ContextMenuService,
   ColumnChooserService,
   ToolbarService,
+  TreeGridComponent,
 } from '@syncfusion/ej2-angular-treegrid';
 import { EditSettingsModel } from '@syncfusion/ej2-treegrid';
+import { BeforeOpenCloseEventArgs } from '@syncfusion/ej2-inputs';
+import { getValue, isNullOrUndefined } from '@syncfusion/ej2-base';
+import { MenuEventArgs } from '@syncfusion/ej2-navigations';
 
 @Component({
   selector: 'app-root',
@@ -28,10 +32,13 @@ import { EditSettingsModel } from '@syncfusion/ej2-treegrid';
 })
 export class AppComponent {
   public data: Object[] = [];
-  public contextMenuItems: string[] = [];
+  public contextMenuItems: any[] = [];
   public editing: EditSettingsModel;
   public toolbar: string[] = ['ColumnChooser'];
   public editparams: Object;
+
+  @ViewChild('treegrid')
+  public treegrid: TreeGridComponent | undefined;
 
   constructor() {
     this.editing = { allowDeleting: true, allowEditing: true, mode: 'Row' };
@@ -41,17 +48,77 @@ export class AppComponent {
   ngOnInit(): void {
     this.data = sampleData;
     this.contextMenuItems = [
-      'AutoFit',
-      'AutoFitAll',
-      'SortAscending',
-      'SortDescending',
-      'Edit',
-      'Delete',
-      'Save',
-      'Cancel',
-      'PdfExport',
-      'ExcelExport',
-      'CsvExport',
+      { text: 'Collapse the Row', target: '.e-content', id: 'collapserow' },
+      { text: 'Expand the Row', target: '.e-content', id: 'expandrow' },
+      { text: 'Collapse All', target: '.e-headercontent', id: 'collapseall' },
+      { text: 'Expand All', target: '.e-headercontent', id: 'expandall' },
     ];
+  }
+
+  contextMenuOpen(arg?: BeforeOpenCloseEventArgs): void {
+    let elem: Element = arg?.event.target as Element;
+    let row: Element | any = elem.closest('.e-row');
+    let uid: string = row && row.getAttribute('data-uid');
+    let items: Array<HTMLElement> = [].slice.call(
+      document.querySelectorAll('.e-menu-item')
+    );
+    for (let i: number = 0; i < items.length; i++) {
+      items[i].setAttribute('style', 'display: none;');
+    }
+    if (elem.closest('.e-row')) {
+      if (
+        isNullOrUndefined(uid) ||
+        isNullOrUndefined(
+          getValue(
+            'hasChildRecords',
+            this.treegrid?.grid.getRowObjectFromUID(uid).data
+          )
+        )
+      ) {
+        (arg as any).cancel = true;
+      } else {
+        let flag: boolean = getValue(
+          'expanded',
+          this.treegrid?.grid.getRowObjectFromUID(uid).data
+        );
+        let val: string = flag ? 'none' : 'block';
+        document
+          .querySelectorAll('li#expandrow')[0]
+          .setAttribute('style', 'display: ' + val + ';');
+        val = !flag ? 'none' : 'block';
+        document
+          .querySelectorAll('li#collapserow')[0]
+          .setAttribute('style', 'display: ' + val + ';');
+      }
+    } else {
+      let len =
+        this.treegrid?.element.querySelectorAll('.e-treegridexpand').length;
+      if (len !== 0) {
+        document
+          .querySelectorAll('li#collapseall')[0]
+          .setAttribute('style', 'display: block;');
+      } else {
+        document
+          .querySelectorAll('li#expandall')[0]
+          .setAttribute('style', 'display: block;');
+      }
+    }
+  }
+  contextMenuClick(args?: MenuEventArgs): void {
+    if (args?.item.id === 'collapserow') {
+      this.treegrid?.collapseRow(
+        this.treegrid.getSelectedRows()[0] as HTMLTableRowElement,
+        this.treegrid.getSelectedRecords()[0]
+      );
+    } else if (args?.item.id === 'expandrow') {
+      this.treegrid?.expandRow(
+        this.treegrid.getSelectedRows()[0] as HTMLTableRowElement,
+        this.treegrid.getSelectedRecords()[0]
+      );
+    } else if (args?.item.id === 'collapseall') {
+      this.treegrid?.collapseAll();
+    } else if (args?.item.id === 'expandall') {
+      this.treegrid?.expandAll();
+    }
   }
 }
